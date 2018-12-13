78b5c40a9e7cf7af95b6b40688d952f84047de1f
knight <tocaldera6734@gmail.com>
2018-12-13 08:23:40 +0530

    Switch component added

--- app/src/main/java/com/example/knight/gcertification/UIElements.java
+++ app/src/main/java/com/example/knight/gcertification/UIElements.java
@@ -2,6 +2,10 @@ package com.example.knight.gcertification;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class UIElements extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class UIElements extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uielements);
+    }
+
+    public void toggleButtonInAction(View view) {
+        Switch aSwitch = findViewById(R.id.switch1);
+        if(aSwitch.isChecked()) {
+            Toast.makeText(this, "Action On", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Switch Off", Toast.LENGTH_SHORT).show();
+        }
     }
 }
